557e4eef6d8fb342b6ec186ea2822761f203b243
Nianaro <AK>
2022-04-19 20:53:27 +0300

    UrlBuilder.cs has been added

---
+++ Solution/Autotests/UrlBuilder.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Autotests;
+
+public static class UrlBuilder
+{
+    public static string GetUrl(Type pageType) =>
+        pageType.GetCustomAttributes<PageUrlAttribute>().Select(a => a.Url).ToArray().First();
+}
