8c969548dbc2f356a72259b5117de6aa65e6c8cc
Nianaro <AK>
2022-04-19 20:53:53 +0300

    PageFactoryBase.cs has been added

---
+++ Solution/Autotests/PageFactoryBase.cs
@@ -0,0 +1,16 @@
+using System;
+using OpenQA.Selenium;
+
+namespace Autotests;
+
+public static class PageFactoryBase
+{
+    public static T Open<T>(IWebDriver driver)
+    {
+        driver.Navigate().GoToUrl(UrlBuilder.GetUrl(typeof(T)));
+        return CreatePage<T>(driver);
+    }
+
+    private static T CreatePage<T>(IWebDriver driver) => (T) Activator.CreateInstance(typeof(T), driver);
+
+}
